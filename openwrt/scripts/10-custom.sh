c3322112a989af494e871efdbdb650191208a793
Jianyun <yihy8023@gmail.com>
2024-11-24 21:52:31 +0800

    config: enable tailscale package and add udpxy support; remove tailscale init script

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
 # 自定义脚本
-# luci-app-webdav
+# OpenClash
 git clone https://$github/vernesong/OpenClash package/new/luci-app-openclash
+# wechatpush
 git clone https://$github/tty228/luci-app-wechatpush package/new/luci-app-wechatpush
 
 
-mkdir -p files/usr/sbin
-curl -skLo files/etc/init.d/tailscale $mirror/openwrt/files/etc/init.d/tailscale
-# https://pkgs.tailscale.com/stable
-if [ "$platform" = "rk3568" ]; then
-    wget https://pkgs.tailscale.com/stable/tailscale_1.76.6_arm64.tgz
-    tar zxvf tailscale_1.76.6_arm64.tgz
-    mv tailscale_1.76.6_arm64/tailscale files/usr/sbin
-    mv tailscale_1.76.6_arm64/tailscaled files/usr/sbin
-else
-    wget https://pkgs.tailscale.com/stable/tailscale_1.76.6_amd64.tgz
-    tar zxvf tailscale_1.76.6_amd64.tgz
-    mv tailscale_1.76.6_amd64/tailscale files/usr/sbin
-    mv tailscale_1.76.6_amd64/tailscaled files/usr/sbin
-fi
 
 
