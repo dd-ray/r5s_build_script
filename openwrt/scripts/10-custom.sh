9fcec5672fe4350037c33279152d46d269e99c3d
Jianyun <yihy8023@gmail.com>
2025-02-12 16:18:13 +0800

    update luci-appl-nikki

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -3,3 +3,4 @@
 # 自定义脚本
 # wechatpush
 git clone https://$github/tty228/luci-app-wechatpush package/new/luci-app-wechatpush
+git clone https://$github/nikkinikki-org/OpenWrt-nikki package/new/OpenWrt-nikki
