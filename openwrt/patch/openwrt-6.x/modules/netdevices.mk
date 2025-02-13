f46e2930b221a3e38cc5724072bfa6b3c6355368
Jianyun <yihy8023@gmail.com>
2025-02-13 09:56:52 +0800

    update kmod depends

--- openwrt/patch/openwrt-6.x/modules/netdevices.mk
+++ openwrt/patch/openwrt-6.x/modules/netdevices.mk
@@ -512,7 +512,7 @@ $(eval $(call KernelPackage,phy-airoha-en8811h))
 define KernelPackage/phy-aquantia
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Aquantia Ethernet PHYs
-  DEPENDS:=+kmod-libphy +kmod-hwmon-core +kmod-lib-crc-ccitt +LINUX_6_12:kmod-lib-crc-itu-t +LINUX_6_13:kmod-lib-crc-itu-t
+  DEPENDS:=+kmod-libphy +kmod-hwmon-core +kmod-lib-crc-ccitt +(LINUX_6_12||LINUX_6_13):kmod-lib-crc-itu-t
   KCONFIG:=CONFIG_AQUANTIA_PHY
   FILES:=$(LINUX_DIR)/drivers/net/phy/aquantia/aquantia.ko
   AUTOLOAD:=$(call AutoLoad,18,aquantia,1)
@@ -1135,7 +1135,7 @@ $(eval $(call KernelPackage,i40e))
 define KernelPackage/iavf
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=Intel(R) Ethernet Adaptive Virtual Function support
-  DEPENDS:=@PCI_SUPPORT +LINUX_6_12:kmod-libie +LINUX_6_12:kmod-libeth +LINUX_6_13:kmod-libie +LINUX_6_13:kmod-libeth
+  DEPENDS:=@PCI_SUPPORT +(LINUX_6_12||LINUX_6_13):kmod-libie +(LINUX_6_12||LINUX_6_13):kmod-libeth
   KCONFIG:= \
        CONFIG_I40EVF \
        CONFIG_IAVF
